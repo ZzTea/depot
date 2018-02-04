002c940925230898b2e5b8ceb9bcf9c145402e64
ZzTea <billsong6919@gmail.com>
2018-02-04 12:52:59 +0800

    Revise a smart cart.

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -1,4 +1,10 @@
 class LineItem < ApplicationRecord
   belongs_to :product
   belongs_to :cart
+
+  def total_price
+    puts "DEUBG product price: #{product.price}" 
+    puts "DEUBG quantity: #{quantity}" 
+    product.price * quantity
+  end
 end
